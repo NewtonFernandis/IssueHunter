e7e1c54ab10f2a02ed2cc32acf742b05df7ac362
NewtonFernandis <newton.fernandis@gmail.com>
2023-09-30 20:45:40 +0530

    WIP issue hunter

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,76 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import React, { useEffect, useState } from 'react'
+import Filters from './components/filters.component'
+import IssueCard from './components/issueCard.component'
+import NavBar from './components/navbar.component'
+import Pagination from './components/pagination.component'
+import Spinner from './components/spinner.component'
+import { Issue } from './types/searchIssueResponse'
+import { githubApi } from './utils/api'
 function App() {
-  const [count, setCount] = useState(0)
+  const [issues, setIssues] = useState<Issue[] | undefined>([])
+  const [currentPage, setCurrentPage] = useState<number>(1)
+  const [loading, setLoading] = useState<boolean>(false)
+
+  const searchQuery = 'is:issue is:open'
+
+  useEffect(() => {
+    getIssues(currentPage)
+  }, [currentPage])
+
+  const getIssues = async (page: number) => {
+    try {
+      setLoading(true)
+      const response = await githubApi.getAllIssues(searchQuery, page)
+      if (response) {
+        const issues: Issue[] = response?.data.items
+        setIssues(issues)
+      }
+      setLoading(false)
+    } catch (error) {
+      setLoading(false)
+      console.log(error)
+    }
+  }
+
+  const nextPage = () => {
+    setCurrentPage(currentPage + 1)
+  }
+
+  const prevPage = () => {
+    if (currentPage > 1) {
+      setCurrentPage(currentPage - 1)
+    }
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <React.Fragment>
+      <NavBar />
+      <Filters />
+
+      <div className="ml-[365px] relative h-screen">
+        {!loading ? (
+          <div className="grid grid-cols-2 gap-2">
+            {issues &&
+              issues.map((issue) => (
+                <div
+                  key={issue.id}
+                  className="w-full"
+                >
+                  <IssueCard issue={issue} />
+                </div>
+              ))}
+          </div>
+        ) : (
+          <Spinner />
+        )}
+
+        <Pagination
+          prevPage={prevPage}
+          currentPage={currentPage}
+          nextPage={nextPage}
+        />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </React.Fragment>
   )
 }
 
---
+++ src/utils/api.ts
@@ -0,0 +1,23 @@
+import { Octokit } from '@octokit/core'
+import { SearchIssuesResponse } from '../types/searchIssueResponse'
+const octokit = new Octokit({
+  auth: import.meta.env.GITHUB_PERSONAL_ACCESS_TOKEN,
+  request: {
+    fetch: fetch,
+  },
+})
+
+export const githubApi = {
+  getAllIssues: async (searchQuery: string, page: number): Promise<any> => {
+    try {
+      return await octokit.request('GET /search/issues', {
+        q: searchQuery,
+        per_page: 20,
+        page: page,
+      })
+    } catch (error) {
+      console.log(error)
+    }
+    return {} as SearchIssuesResponse
+  },
+}
